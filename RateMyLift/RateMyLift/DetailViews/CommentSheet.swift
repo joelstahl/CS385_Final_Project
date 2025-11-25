91cd0eb9e8441e2355c1937bec4d511b792429f6
dmyles12356 <c26devyn.myles@afacademy.af.edu>
2025-11-25 09:06:38 -0700

    Added CommentSheet and Rating

---
+++ RateMyLift/RateMyLift/DetailViews/CommentSheet.swift
@@ -0,0 +1,18 @@
+//
+//  CommentSheet.swift
+//  RateMyLift
+//
+//  Created by Devyn Myles on 11/25/25.
+//
+
+import SwiftUI
+
+struct CommentSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentSheet()
+}
---
+++ RateMyLift/RateMyLift/DetailViews/RateWorkout.swift
@@ -0,0 +1,18 @@
+//
+//  RateWorkout.swift
+//  RateMyLift
+//
+//  Created by Devyn Myles on 11/25/25.
+//
+
+import SwiftUI
+
+struct RateWorkout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RateWorkout()
+}
